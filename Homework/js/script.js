0f9ee4d513b30183dc41c6cf9bff97ac76fb87a8
SwitchTia <oblasser.tatiana@gmail.com>
2025-10-29 09:53:46 +0100

    Fix: Add variable result to centralize the output

--- Homework/js/script.js
+++ Homework/js/script.js
@@ -34,17 +34,21 @@
 
 //***RUN PROGRAM***/
 
+
+
 for(let i = 1; i <= 100; i++){
-    if( i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
+    let result = "";
+    if(i % 3 === 0 && i % 5 === 0){
+        result = "FizzBuzz";
     }
-    else if( i % 3 === 0){
-        console.log("Fizz");
+    else if(i % 3 === 0){
+        result ="Fizz";
     }
-    else if( i % 5 === 0){
-        console.log("Buzz");
+    else if(i % 5 === 0){
+        result = "Buzz";
     }
     else {
-        console.log(i);
+        result = i;
     }
+    console.log(result);
 }
